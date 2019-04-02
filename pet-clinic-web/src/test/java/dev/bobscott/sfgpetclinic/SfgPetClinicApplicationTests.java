75e63116c4b8a7a09212bfc439de1a2f94477daa
Bob <bob@rwscott.co.uk>
2019-04-02 09:59:06 +0100

    Convert to Maven multi-module. Fixes #1.

--- pet-clinic-web/src/test/java/dev/bobscott/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dev/bobscott/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,11 +1,13 @@
 package dev.bobscott.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
