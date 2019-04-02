f1e10384a990cc5bf9c71f11d8cba401b72118e1
Bob <bob@rwscott.co.uk>
2019-04-02 10:32:58 +0100

    Implement service interfaces. Fixes #5.

---
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package dev.bobscott.sfgpetclinic.services;
+
+import dev.bobscott.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findById(Long id);
+    Owner findByLastName(String lastName);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package dev.bobscott.sfgpetclinic.services;
+
+import dev.bobscott.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet owner);
+    Set<Vet> findAll();
+}
