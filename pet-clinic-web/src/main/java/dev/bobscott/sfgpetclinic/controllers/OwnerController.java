e371c9669b4abc04547b09b4a5dd8b5a0c214841
Bob <bob@rwscott.co.uk>
2019-04-03 10:45:37 +0100

    Implement owner index page and controller. Fixes #13.

---
+++ pet-clinic-web/src/main/java/dev/bobscott/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package dev.bobscott.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
