2e951fe63111e5bf7ceff28f240bb8071373b68b
Bob <bob@rwscott.co.uk>
2019-04-05 13:08:52 +0100

    Fix broken menu links: add notimplemented route and custom page. Redirect unimplemented routes to /notimplemented.

--- pet-clinic-web/src/main/java/dev/bobscott/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/dev/bobscott/sfgpetclinic/controllers/IndexController.java
@@ -2,6 +2,7 @@ package dev.bobscott.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,15 @@ public class IndexController {
     @RequestMapping({"","/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public RedirectView oups() {
+        return new RedirectView("notimplemented");
+    }
+
+    @RequestMapping("/notimplemented")
+    public String notImplemented() {
+        return "notimplemented";
     }
 }
--- pet-clinic-web/src/main/java/dev/bobscott/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/dev/bobscott/sfgpetclinic/controllers/OwnerController.java
@@ -4,6 +4,7 @@ import dev.bobscott.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/owners")
@@ -19,5 +20,10 @@ public class OwnerController {
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("find")
+    public RedirectView findOwners() {
+        return new RedirectView("/notimplemented");
     }
 }
