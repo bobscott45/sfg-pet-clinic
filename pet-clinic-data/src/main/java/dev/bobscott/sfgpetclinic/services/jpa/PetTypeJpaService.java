2b698fb652a44665ab2a3021377dac6388869050
Bob <bob@rwscott.co.uk>
2019-04-08 14:03:42 +0100

    Add  @Service and @Profile annotation to PetTypeJpaServcie

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/PetTypeJpaService.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/PetTypeJpaService.java
@@ -3,9 +3,13 @@ package dev.bobscott.sfgpetclinic.services.jpa;
 import dev.bobscott.sfgpetclinic.model.Pet;
 import dev.bobscott.sfgpetclinic.repositories.PetRepository;
 import dev.bobscott.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
+@Profile("jpa")
 public class PetTypeJpaService implements PetService {
 
     private final PetRepository petRepository;
