8c98e4a9a5ad6b60220344987faa282b8faa2974
Bob <bob@rwscott.co.uk>
2019-04-08 14:05:31 +0100

    Implement PetJpaService

---
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/PetJpaService.java
@@ -0,0 +1,45 @@
+package dev.bobscott.sfgpetclinic.services.jpa;
+
+import dev.bobscott.sfgpetclinic.model.Pet;
+import dev.bobscott.sfgpetclinic.repositories.PetRepository;
+import dev.bobscott.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return petRepository.findAll();
+    }
+}
