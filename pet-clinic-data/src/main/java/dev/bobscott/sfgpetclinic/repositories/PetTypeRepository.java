e6998eb7e745375c306664d42ff55d294ba5ceb8
Bob <bob@rwscott.co.uk>
2019-04-08 14:58:14 +0100

    Override findAll in PetTypeRepository to return Set<PetType>

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/PetTypeRepository.java
@@ -3,5 +3,9 @@ package dev.bobscott.sfgpetclinic.repositories;
 import dev.bobscott.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface PetTypeRepository extends CrudRepository<PetType, Long> {
+    @Override
+    Set<PetType> findAll();
 }
