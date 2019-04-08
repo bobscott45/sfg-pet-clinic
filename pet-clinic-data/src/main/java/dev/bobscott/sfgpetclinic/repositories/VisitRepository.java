08ffb10be0e8227f06966156f545df92d66010d1
Bob <bob@rwscott.co.uk>
2019-04-08 15:02:59 +0100

    Implemet VisitJpaService. Closes #47.

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/VisitRepository.java
@@ -3,5 +3,9 @@ package dev.bobscott.sfgpetclinic.repositories;
 import dev.bobscott.sfgpetclinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface VisitRepository extends CrudRepository<Visit, Long> {
+    @Override
+    Set<Visit> findAll();
 }
---
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,41 @@
+package dev.bobscott.sfgpetclinic.services.jpa;
+
+import dev.bobscott.sfgpetclinic.model.Visit;
+import dev.bobscott.sfgpetclinic.repositories.VisitRepository;
+import dev.bobscott.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return visitRepository.findAll();
+    }
+}
