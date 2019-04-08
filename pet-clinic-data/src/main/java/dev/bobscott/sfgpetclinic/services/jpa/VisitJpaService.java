1f249e73b9e4ca36d372c09301d8653370f15a15
Bob <bob@rwscott.co.uk>
2019-04-08 16:02:34 +0100

    Implement createPetVisit

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/VisitJpaService.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/VisitJpaService.java
@@ -1,11 +1,17 @@
 package dev.bobscott.sfgpetclinic.services.jpa;
 
+import dev.bobscott.sfgpetclinic.model.Pet;
 import dev.bobscott.sfgpetclinic.model.Visit;
 import dev.bobscott.sfgpetclinic.repositories.VisitRepository;
 import dev.bobscott.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Set;
 
+@Service
+@Profile("jpa")
 public class VisitJpaService implements VisitService {
 
     private final VisitRepository visitRepository;
@@ -37,5 +43,15 @@ public class VisitJpaService implements VisitService {
     @Override
     public Set<Visit> findAll() {
         return visitRepository.findAll();
+    }
+
+    @Override
+    public void createPetVisit(Pet pet, LocalDate date, String description) {
+        Visit visit = new Visit();
+        visit.setDate(date);
+        visit.setDescription(description);
+        visit.setPet(pet);
+        save(visit);
+        pet.getVisits().add(visit);
     }
 }
