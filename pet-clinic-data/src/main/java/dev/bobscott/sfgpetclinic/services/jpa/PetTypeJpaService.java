bd4f04a12b7480894fee48471da654a2ff2a39c8
Bob <bob@rwscott.co.uk>
2019-04-08 14:56:36 +0100

    Fix PetTypeJpaService implementing wrong interface.

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/PetTypeJpaService.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/jpa/PetTypeJpaService.java
@@ -1,8 +1,8 @@
 package dev.bobscott.sfgpetclinic.services.jpa;
 
-import dev.bobscott.sfgpetclinic.model.Pet;
-import dev.bobscott.sfgpetclinic.repositories.PetRepository;
-import dev.bobscott.sfgpetclinic.services.PetService;
+import dev.bobscott.sfgpetclinic.model.PetType;
+import dev.bobscott.sfgpetclinic.repositories.PetTypeRepository;
+import dev.bobscott.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -10,37 +10,36 @@ import java.util.Set;
 
 @Service
 @Profile("jpa")
-public class PetTypeJpaService implements PetService {
+public class PetTypeJpaService implements PetTypeService {
 
-    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
 
-    public PetTypeJpaService(PetRepository petRepository) {
-        this.petRepository = petRepository;
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
-    public Pet findById(Long id) {
-        return petRepository.findById(id).orElse(null);
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
     }
 
     @Override
-    public Pet save(Pet object) {
-        return petRepository.save(object);
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
     }
 
     @Override
-    public void delete(Pet object) {
-        petRepository.delete(object);
-
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long id) {
-        petRepository.deleteById(id);
+        petTypeRepository.deleteById(id);
     }
 
     @Override
-    public Set<Pet> findAll() {
-        return petRepository.findAll();
+    public Set<PetType> findAll() {
+        return petTypeRepository.findAll();
     }
 }
