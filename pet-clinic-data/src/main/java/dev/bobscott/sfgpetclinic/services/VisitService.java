eaca8d560a7b465c11301ed21b5488c1ff458e11
Bob <bob@rwscott.co.uk>
2019-04-08 16:00:54 +0100

    Add createPetVisit method

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/VisitService.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/VisitService.java
@@ -1,6 +1,11 @@
 package dev.bobscott.sfgpetclinic.services;
 
+import dev.bobscott.sfgpetclinic.model.Pet;
 import dev.bobscott.sfgpetclinic.model.Visit;
 
+import java.time.LocalDate;
+
 public interface VisitService extends CrudService<Visit, Long> {
+
+    void createPetVisit(Pet pet, LocalDate date, String description);
 }
