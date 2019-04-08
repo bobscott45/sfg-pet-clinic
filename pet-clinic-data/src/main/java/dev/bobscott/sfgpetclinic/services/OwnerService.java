b2680f460826a6e964a01b79a41b9ea60767409e
Bob <bob@rwscott.co.uk>
2019-04-08 14:50:08 +0100

    Removed @Service and @Profile from OwnerService interface.

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/services/OwnerService.java
@@ -1,11 +1,7 @@
 package dev.bobscott.sfgpetclinic.services;
 
 import dev.bobscott.sfgpetclinic.model.Owner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service
-@Profile({"default", "map"})
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
