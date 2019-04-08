30631a7e1a3a31022fb035c3c79763245de9195b
Bob <bob@rwscott.co.uk>
2019-04-08 14:57:13 +0100

    Add blank lines.

--- pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/dev/bobscott/sfgpetclinic/repositories/OwnerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Set;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
     Owner findByLastName(String lastName);
 
     @Override
