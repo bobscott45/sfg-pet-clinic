fe26b0e2683a3e6e7f08ce3be21e2fbfaf11e9e3
Bob <bob@rwscott.co.uk>
2019-04-11 15:28:43 +0100

    Update to JUnit5. Closes #4.

---
+++ pet-clinic-data/src/test/java/dev/bobscott/sfgpetclinic/repositories/OwnerRepositoryTest.java
@@ -0,0 +1,12 @@
+package dev.bobscott.sfgpetclinic.repositories;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerRepositoryTest {
+
+    @Test
+    void justAnExample() {
+
+    }
+
+}
--- pet-clinic-web/src/test/java/dev/bobscott/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dev/bobscott/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,11 @@
 package dev.bobscott.sfgpetclinic;
 
-
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
